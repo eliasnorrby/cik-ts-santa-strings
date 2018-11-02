ea925b75e3ce5eec030cab3e718b836535c2bfcb
Elias Norrby <elias.norrby@gmail.com>
2018-11-02 12:46:26 +0100

    Implement CriterionVowel

--- test/CriterionVowelTest.ts
+++ test/CriterionVowelTest.ts
@@ -2,6 +2,25 @@ import { CriterionVowel } from "../src/CriterionVowel";
 
 describe('CriterionVowel', () => {
     
-    let c = new CriterionVowel();
+    let c: CriterionVowel;
 
+    let ns1 = "aao";
+    let ns2 = "hdjjshgkaou";
+    let ns3 = "hjdjajfkdjfoldkdjfkjdfo";
+
+    beforeEach(() => {
+        c = new CriterionVowel();
+    })
+
+    it('should return true for nice string', () => {
+        expect(c.check(ns1)).toBeTruthy();
+    })
+    
+    it('should return true for long nice string', () => {
+        expect(c.check(ns2)).toBeTruthy();
+    })
+
+    it('should return true for long nice string with vowels separated', () => {
+        expect(c.check(ns3)).toBeTruthy();
+    })
 })
