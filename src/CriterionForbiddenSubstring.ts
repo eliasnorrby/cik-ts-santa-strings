0a4d2f3c4a5a2252657755fb532a9ba0315fcacc
Elias Norrby <elias.norrby@gmail.com>
2018-11-02 13:08:00 +0100

    Implement CriterionForbiddenSubstring

---
+++ src/CriterionForbiddenSubstring.ts
@@ -0,0 +1,20 @@
+import { ICriterion } from "./ICriterion";
+
+export class CriterionForbiddenSubstring implements ICriterion {
+    
+    forbiddenSubstrings = [
+        "ab", 
+        "cd",
+        "pq",
+        "xy"
+    ]
+
+    check(string: string): boolean {
+        let includesForbiddenString = false;
+        this.forbiddenSubstrings.forEach(fs => {
+            includesForbiddenString = string.includes(fs);
+        })
+        return !includesForbiddenString;
+    }
+
+}
